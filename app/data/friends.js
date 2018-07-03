9214d975aa633b1e9b02e97affb5d05aa23c24e7
Dina <>
2018-07-03 18:27:56 -0500

    friends array updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,29 +9,29 @@ var friendsData = [
     photo:"https://truththeory.com/wp-content/uploads/2017/07/study-shows-smart-people-are-far.jpg",
     scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
   },
-  { //https://cdn.pixabay.com/photo/2015/01/15/12/46/model-600225_1280.jpg
+  { 
     name: "The numberTwo",
-    photo:"https://avatars1.githubusercontent.com/u/12567554?s=400&u=6ea987b3937c8dc10581b8c102f60b727d33ad13&v=4",
+    photo:"http://profilepicturesdp.com/wp-content/uploads/2017/02/Christmas-DP-for-whatsapp-images-127.jpg",
     scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   },
   {
     name: "The numberThree",
-    photo:"https://cdn.pixabay.com/photo/2015/11/26/00/14/fashion-1063100_1280.jpg",
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ3cF4MlqomtuIptSvbEn3QQyhlmdNEB-SAFDzXA7OcdfR2LziVw",
     scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
   },
   {
     name: "The numberFour",
-    photo:"https://cdn.pixabay.com/photo/2016/04/10/21/34/woman-1320810_1280.jpg",
+    photo:"https://truththeory.com/wp-content/uploads/2017/07/study-shows-smart-people-are-far.jpg",
     scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
   },
   {
     name: "The numberFive",
-    photo:"https://cdn.pixabay.com/photo/2015/07/09/00/29/woman-837156_1280.jpg",
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ3cF4MlqomtuIptSvbEn3QQyhlmdNEB-SAFDzXA7OcdfR2LziVw",
     scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
   },
   {
     name: "The numberSix",
-    photo:"https://cdn.pixabay.com/photo/2017/08/01/08/29/people-2563491_1280.jpg",
+    photo:"http://profilepicturesdp.com/wp-content/uploads/2017/02/Christmas-DP-for-whatsapp-images-127.jpg",
     scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
   
   }
