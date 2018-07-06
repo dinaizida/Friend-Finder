2073e25bf9c105a8ca9826a786a69a08ced31b78
Dina <>
2018-07-05 22:20:49 -0500

    bugs for calculation fixed

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,3 @@
-
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a  "data" source. 
@@ -8,57 +7,62 @@ var friends = require('../data/friends.js');
 // ===============================================================================
 // ROUTING
 // ===============================================================================
-module.exports = function(app){
+module.exports = function(app) {
     //A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
     // a POST route to get a userInput data from the survey form and calculate the best match
-    app.post('/api/friends', function(req, res){
+    app.post('/api/friends', function(req, res) {
+
         // Capture the user input object
-            var userInput = req.body;
-            console.log('userInput = ' + JSON.stringify(userInput));
-            var userName = userInput.name;
-            var userPhoto = userInput.photo;
-            var userScores = userInput.scores;
-            console.log('userScores = ' + userScores);
-            var differenceArray = [];//to store scores differents for each person in the database and a new user
-            
-            //bestMatch object
-            var bestMatch = {
-                name: "",
-                photo: "",
-                friendDifference: 0
-            };
-            
-            // check every person in the friends array
-            for(var i = 0; i < friends.length; i++){
-                console.log(friends[i].name);
-                var difference = 0;
-                // check scores for all 10 questions for each person in the friends array
-                for (var j = 0; j < friends[i].scores[j]; j++){
-                //calculate the difference between the scores and store them in the array
-                difference += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j])));
-                console.log('difference = ' + difference);
-                }
-                differenceArray.push(difference);
-                console.log('difference Array = ' + differenceArray);
+        var newUserInput = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: []
+        };
+        console.log('new User Input name - ' + req.body.name);
+        // store newUserInput scores in the array
+        var scoresArray = [];
+        for (var i = 0; i < req.body.scores.length; i++) {
+            scoresArray.push(parseInt(req.body.scores[i]))
+        }
+        newUserInput.scores = scoresArray;
+        console.log('new User Input Scores  Array = ' + scoresArray);
+
+        //  check the new User Input entry with the existing ones
+        var scoreDiffArray = [];
+        for (var i = 0; i < friends.length; i++) {
+
+            // Check each friend's scores and summarize the difference 
+            var scoreDiff = 0;
+            for (var j = 0; j < newUserInput.scores.length; j++) {
+                scoreDiff += Math.abs(newUserInput.scores[j] - friends[i].scores[j]);
+                console.log('scoreDifference = ' + scoreDiff + "  j = " + j);
+            }
+            // summ of differences for each friend and new User store in the array
+            scoreDiffArray.push(scoreDiff);
+            console.log('scoreDiffArray for each object in the friends array = ' + scoreDiffArray + " i = " + i);
+        }
+        console.log('scoreDiffArray TOTAL = ' + scoreDiffArray);
+
+        // Determine the best match using the postion of the best match in the friends array
+        var bestMatchPosition = 0;
+        for (var i = 1; i < scoreDiffArray.length; i++) {
+
+            if (scoreDiffArray[i] <= scoreDiffArray[bestMatchPosition]) {
+                bestMatchPosition = i;
+
             }
 
-            //find the best match (find min in the array)
-            for (var i = 0; i < differenceArray.length; i++){
-                if(differenceArray[i] <= bestMatch.friendDifference){
-                    friendDifference =i;
-                }
-            }
-            //return bestMatch data
-            var bestFriendMatch = friends[friendDifference];
-            res.json(bestFriendMatch);
-            
-            //pushes the new userInput into the friends array
-            friends.push(req.body);
+        }
+        //return best Match data
+        var bestFriendMatch = friends[bestMatchPosition];
+        res.json(bestFriendMatch);
+
+        //pushes the new userInput into the friends array
+        friends.push(newUserInput);
 
     });
-
 
 };
